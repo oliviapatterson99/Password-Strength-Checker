933c964f38adb653fc58ba772eb0788d385fcc26
olivia <oliviapatterson123@live.com>
2026-02-24 16:58:45 +0000

    Change regex validation

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,26 +1,26 @@
 class Password < ApplicationRecord
     def is_strong?
-        return true if self.characters.match?("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")
+        return true if self.characters.match?(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
         false
     end
 
     def contains_number?
-        return true if self.characters.match?("^(?=.*\d)")
+        return true if self.characters.match?(/^(?=.*?[0-9])/)
         false
     end
 
     def contains_lowercase?
-        return true if self.characters.match?("^(?=.*[a-z])")
+        return true if self.characters.match?(/^(?=.*?[a-z])/)
         false
     end
 
     def contains_uppercase?
-        return true if self.characters.match?("^(?=.*[A-Z])")
+        return true if self.characters.match?(/^(?=.*?[A-Z])/)
         false
     end
 
     def contains_special_character?
-        return true if self.characters.match?("^(?=.*[@$!%*?&])")
+        return true if self.characters.match?(/^(?=.*?[#?!@$%^&*-])/)
         false
     end
     
